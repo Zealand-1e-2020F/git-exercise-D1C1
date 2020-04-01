78ee880515318c9abfa5122dcfd0f78230385ca4
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-04-01 11:38:10 +0200

    Ny feature by
    added instance field,property og værdi i constructor til variabel by

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace GitExercise
         {
             Console.WriteLine("Hello World!");
 
-            myClass myclass = new myClass("Bent","hejvej2");
-            myClass myClass1 = new myClass("Børge","hejvej2");
-            myClass myClass2 = new myClass("Lasse","hejvej2");
+            myClass myclass = new myClass("Bent","hejvej2","Nykøbing sj");
+            myClass myClass1 = new myClass("Børge","hejvej2","Nykøbing sj");
+            myClass myClass2 = new myClass("Lasse","hejvej2","Nykøbing sj");
         }
     }
 }
--- myClass.cs
+++ myClass.cs
@@ -5,17 +5,20 @@ namespace GitExercise
         private string navn;
         private string adresse;
         private int postnr = 4500;
-        public myClass(string navn,string adresse)
+        private string by;
+        public myClass(string navn,string adresse,string by)
         {
             this.Navn = navn;
             this.Adresse = adresse;
+            this.By=by;
         }
         public string Navn { get => navn; set => navn = value; }
         public string Adresse { get => adresse; set => adresse = value; }
         public int Postnr { get => postnr; set => postnr = value; }
+        public string By { get => by; set => by = value; }
 
         public override string ToString(){
-            return $"{Navn} bor på {Adresse},{Postnr}";
+            return $"{Navn} bor på {Adresse},{Postnr},{By}";
         }
     }
 }
