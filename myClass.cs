d2c657482e1c70748ad2954824b129abd5045d2f
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-04-01 11:11:34 +0200

    added adresse/postnr
    new instance field  og property til adresse og postnr

--- myClass.cs
+++ myClass.cs
@@ -3,10 +3,14 @@ namespace GitExercise
     class myClass
     {
         private string navn;
+        private string adresse;
+        private int postnr;
         public myClass(string navn)
         {
             this.Navn = navn;
         }
         public string Navn { get => navn; set => navn = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public int Postnr { get => postnr; set => postnr = value; }
     }
 }
