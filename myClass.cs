50c5edf857ffd1338f60537b448f60c646215d1e
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2020-04-01 10:43:56 +0200

    Added Navn + constructor
    added instance field and property for navn + made a constructor for the class

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,12 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        private string navn;
+        public myClass(string navn)
+        {  
+            this.Navn=navn;
+        }
+
+        public string Navn { get => navn; set => navn = value; }
     }
 }
